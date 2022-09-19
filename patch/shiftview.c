26b7919d445515408db295a2f0bccd36a35994f0
Akash Vishwakarma <72139935+akashisgreat@users.noreply.github.com>
2022-09-19 19:18:34 +0530

    Add files via upload

---
+++ patch/shiftview.c
@@ -0,0 +1,21 @@
+/** Function to shift the current view to the left/right
+ *
+ * @param: "arg->i" stores the number of tags to shift right (positive value)
+ *          or left (negative value)
+ */
+// Put this function in dwm.c with below one
+// and with this >> void shiftview(const Arg *arg); // at top
+void
+shiftview(const Arg *arg) {
+	Arg shifted;
+
+	if(arg->i > 0) // left circular shift
+		shifted.ui = (selmon->tagset[selmon->seltags] << arg->i)
+		   | (selmon->tagset[selmon->seltags] >> (LENGTH(tags) - arg->i));
+
+	else // right circular shift
+		shifted.ui = selmon->tagset[selmon->seltags] >> (- arg->i)
+		   | selmon->tagset[selmon->seltags] << (LENGTH(tags) + arg->i);
+
+	view(&shifted);
+}
